15e774dd6c761d14c21534b470d7783024ef15cc
JhorGy <jorge.mendezj@cecar.edu.co>
2025-03-28 09:51:25 -0500

    Se añaden modificaciones

--- mf-react/src/team-react-mf-react.tsx
+++ mf-react/src/team-react-mf-react.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
 import Root from "./root.component";
+import "antd/dist/antd.css";
+import "./styles/global.css"; // Importar estilos globales
+
 
 const lifecycles = singleSpaReact({
   React,
   ReactDOM,
   rootComponent: Root,
   errorBoundary(err, info, props) {
-    // Customize the root error boundary for your microfrontend here.
-    return null;
+    return <div>Ocurrió un error en la aplicación</div>;
   },
 });
 
