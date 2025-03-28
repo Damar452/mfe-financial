f4edc1686edc3284db64d2c9b00848163198aa29
Damar Hernandez <damar.hernandez@bitakora.com.co>
2025-03-28 11:21:58 -0500

    Add angular routes

--- mf-angular/team-angular/src/app/app.module.ts
+++ mf-angular/team-angular/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AppRoutingModule } from './app-routing.module';
+import { BankAccountManagementComponent } from './pages/bank-account-management/bank-account-management.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BankAccountManagementComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ mf-angular/team-angular/src/app/pages/bank-account-management/bank-account-management.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bank-account-management',
+  templateUrl: './bank-account-management.component.html',
+  styleUrls: ['./bank-account-management.component.scss']
+})
+export class BankAccountManagementComponent {
+
+}
